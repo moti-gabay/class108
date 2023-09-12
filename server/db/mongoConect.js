3317ac1ad33703a90ecbd4ff90b30d0f0334cff8
Moti Gabay <129662657+moti-gabay@users.noreply.github.com>
2023-09-12 10:36:32 +0300

    ok

---
+++ server/db/mongoConect.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect("");
+  console.log(`mongo connect class_108 atlas `);
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
