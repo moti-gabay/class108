49414d1925206c551281dc105f6e51cd1f6b66c3
Moti Gabay <129662657+moti-gabay@users.noreply.github.com>
2023-09-12 11:12:07 +0300

    ok

--- server/models/picassoModel.js
+++ server/models/picassoModel.js
@@ -1,15 +1,13 @@
 const mongoose = require("mongoose");
-const jwt = require("jsonwebtoken");
-const { config } = require("../config/secret");
-
-const LinksSchema = new mongoose.Schema({
+const { Joi } = require("joi")
+const picassoSchema = new mongoose.Schema({
   name:String,
   url:String,
 });
 
-exports.LinksModel = mongoose.model("links",LinksSchema);
+exports.PicassoModel = mongoose.model("picasso",picassoSchema);
 
-exports.validLink = (reqBody) => {
+exports.validPicasso = (reqBody) => {
     const joiSchema = Joi.object({
       name:Joi.string().min(2).max(100).required(),
       url:Joi.string().min(3).max(9999).required()
--- server/routes/configRoutes.js
+++ server/routes/configRoutes.js
@@ -1,7 +1,9 @@
 const indexR = require("./index");
+const picassoR = require("./picasso");
 
 
 exports.routesInit = (app) => {
    app.use("/",indexR);
+   app.use("/picasso",picassoR);
    
 }
