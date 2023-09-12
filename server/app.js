9a22c0a5eb92ad4db03b5b044f21d8aee3411121
Moti Gabay <129662657+moti-gabay@users.noreply.github.com>
2023-09-12 10:52:05 +0300

    ok

---
+++ server/app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const http = require("http");
+const path = require("path");
+const {routesInit} = require("./routes/configRoutes");
+const cors = require("cors")
+require("./db/mongoConnect");
+
+const app = express();
+// prevent cors problem
+app.use(cors());
+app.use(express.json());
+app.use(express.static(path.join(__dirname,"public")));
+routesInit(app);
+const server = http.createServer(app);
+const port = 3003;
+server.listen(port);
---
+++ server/routes/configRoutes.js
@@ -0,0 +1,7 @@
+const indexR = require("./index");
+
+
+exports.routesInit = (app) => {
+   app.use("/",indexR);
+   
+}
